acdbfe4953202aaeedf4207eb39c7e1d7300e262
ShraiLakshmanR <53818678+ShraiLakshmanR@users.noreply.github.com>
2019-09-11 12:24:34 +0530

    Create (lAB 5-a)sud.cpp

---
+++ (lAB 5-a)sud.cpp
@@ -0,0 +1,191 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std; 
+  
+ 
+#define UNASSIGNED 0  
+#define N 9  
+
+
+
+
+
+bool FindUnassignedLocation(int grid[N][N],  
+                            int &row, int &col);  
+  
+
+
+
+
+
+bool isSafe(int grid[N][N], int row, 
+                   int col, int num);  
+  
+
+
+
+
+
+
+
+
+
+
+bool SolveSudoku(int grid[N][N])  
+{  
+    int row, col;  
+  
+    // If there is no unassigned location, 
+    // we are done  
+    if (!FindUnassignedLocation(grid, row, col))  
+    return true; // success!  
+  
+    // consider digits 1 to 9  
+    for (int num = 1; num <= 9; num++)  
+    {  
+        // if looks promising  
+        if (isSafe(grid, row, col, num))  
+        {  
+            // make tentative assignment  
+            grid[row][col] = num;  
+  
+            // return, if success, yay!  
+            if (SolveSudoku(grid))  
+                return true;  
+  
+            						// failure, unmake & try again  
+            grid[row][col] = UNASSIGNED;  
+        }  
+    }  
+    return false; 
+}  
+  
+
+
+
+
+
+
+
+
+
+
+
+bool FindUnassignedLocation(int grid[N][N],  
+                            int &row, int &col)  
+{  
+    for (row = 0; row < N; row++)  
+        for (col = 0; col < N; col++)  
+            if (grid[row][col] == UNASSIGNED)  
+                return true;  
+    return false;  
+}  
+  
+
+
+
+
+
+
+bool UsedInRow(int grid[N][N], int row, int num)  
+{  
+    for (int col = 0; col < N; col++)  
+        if (grid[row][col] == num)  
+            return true;  
+    return false;  
+}  
+  
+
+
+
+
+
+
+
+bool UsedInCol(int grid[N][N], int col, int num)  
+{  
+    for (int row = 0; row < N; row++)  
+        if (grid[row][col] == num)  
+            return true;  
+    return false;  
+}  
+  
+
+
+
+
+
+
+
+bool UsedInBox(int grid[N][N], int boxStartRow,int boxStartCol, int num)  
+{  
+    for (int row = 0; row < 3; row++)  
+        for (int col = 0; col < 3; col++)  
+            if (grid[row + boxStartRow] 
+                    [col + boxStartCol] == num)  
+                return true;  
+    return false;  
+}  
+  
+
+
+
+
+
+
+
+
+
+bool isSafe(int grid[N][N], int row,int col, int num)  
+{  
+    
+    return !UsedInRow(grid, row, num) &&  
+           !UsedInCol(grid, col, num) &&  
+           !UsedInBox(grid, row - row % 3,col - col % 3, num) &&  
+            grid[row][col] == UNASSIGNED;  
+}  
+  
+
+
+
+
+
+
+void printGrid(int grid[N][N])  
+{  
+    for (int row = 0; row < N; row++)  
+    {  
+    for (int col = 0; col < N; col++)  
+            cout << grid[row][col] << " ";  
+        cout << endl; 
+    }  
+}  
+  
+
+
+
+
+
+
+
+
+
+
+int main()  
+{  
+    
+int grid[N][N]; 
+cout<<"Enter the values of suduko\n";
+for(int i=0;i<N;i++)
+	for(int j=0;j<N;j++)
+		{
+		cin>>grid[i][j];
+		}
+
+    if (SolveSudoku(grid) == true)  
+        printGrid(grid);  
+    else
+        cout << "No solution exists";  
+  
+    return 0;  
+}  
