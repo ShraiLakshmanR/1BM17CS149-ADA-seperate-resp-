8beeb5b6efcd9c79eddcc38624fc6a95c028cb54
ShraiLakshmanR <53818678+ShraiLakshmanR@users.noreply.github.com>
2019-10-23 12:59:31 +0530

    Create (LAB-8b)kthelerange.cc

---
+++ (LAB-8b)kthelerange.cc
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h> 
+
+using namespace std; 
+int ptr[501]; 
+void findSmallestRange(int **arr, int n, int k) 
+{ 
+	int i,minval,maxval,minrange,minel,maxel,flag,minind; 
+		
+	for(i = 0;i <= k;i++) 
+		ptr[i] = 0; 
+
+	minrange = INT_MAX; 
+		
+	while(1)	 
+	{ 
+		minind = -1; 
+		minval = INT_MAX; 
+		maxval =	 INT_MIN; 
+		flag = 0; 
+
+		
+		for(i = 0;i < k;i++) 
+		{	 
+				
+			if(ptr[i] == n) 
+			{ 
+				flag = 1; 
+				break; 
+			} 
+			
+			if(ptr[i] < n && arr[i][ptr[i]] < minval) 
+			{ 
+				minind=i;
+				minval=arr[i][ptr[i]]; 
+			} 
+			
+			if(ptr[i] < n && arr[i][ptr[i]] > maxval)	 
+			{ 
+				maxval = arr[i][ptr[i]]; 
+			} 
+		} 
+
+		//Program exited with return code: 0
+
+		if(flag) 
+			break; 
+
+		ptr[minind]++; 
+
+
+		if((maxval-minval) < minrange) 
+		{ 
+			minel = minval; 
+			maxel = maxval; 
+			minrange = maxel - minel; 
+		} 
+	} 
+		
+	printf("The smallest range is [%d , %d]\n",minel,maxel); 
+}	 
+
+int main() 
+{
+	int k,N;
+	cout<<"ENTER THE VALUE OF K:\n ";
+	cin>>k;
+	cout<<"\nENTER THE SIZE OF THE ARRAY:\n";
+	cin>>N;
+	int **arr=new int*[k];
+	for(int i=0;i<k;i++)
+		arr[i]=new int[N];
+	cout<<"\nENTER THE ARRAY ELEMENTS:\n";
+	for(int i=0;i<k;i++)
+		for(int j=0;j<N;j++)
+			cin>>arr[i][j];
+
+	findSmallestRange(arr,N,k); 
+
+	return 0; 
+} 
+
+
+/*ENter value of K=2
+
+Enter size of array =4
+
+78
+13
+12
+16
+5
+23
+13
+5
+
+*/
